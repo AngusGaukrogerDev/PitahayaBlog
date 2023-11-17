a6c92610b88ba3ccb2e91de6c46c676e2cf9a1a7
Angus Gaukroger <agaukroger77@gmail.com>
2023-11-17 21:30:33 +0000

    removed render

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 import BlogHomePage from "@/components/blog-home-page"
 import Navbar from "@/components/navbar"
 import SectionHeader from "@/components/sectionHeader"
-import { render } from "react-dom"
 export default function Home() {
     return(
       <div className="h-full w-full flex flex-col justify-center items-center bg-whitey-default mx-auto px-5 ">
