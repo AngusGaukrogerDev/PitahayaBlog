55e9a911f65e1eb1f76bd4c8f5b8e6b6dc88c7ab
Angus Gaukroger <agaukroger77@gmail.com>
2023-11-19 19:40:21 +0000

    Formatted home page a little

--- components/postPreview.js
+++ components/postPreview.js
@@ -13,10 +13,10 @@ const PostPreview = ({
   }) => {
     return (
         <div>
-          <div className="mb-5">
+          <div className="my-5">
             <CoverImage slug={slug} title={title} src={coverImage} />
           </div>
-          <h3 className="text-3xl mb-3 leading-snug">
+          <h3 className="text-3xl mb-3 leading-snug h-32">
             <Link
               as={`/posts/${topic}/${slug}`}
               href="posts/[slug]"
@@ -28,7 +28,7 @@ const PostPreview = ({
           <div className="text-lg mb-4">
             <DateFormatter dateString={date} />
           </div>
-          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+          {/* <p className="text-lg leading-relaxed mb-4">{excerpt}</p> */}
           <Avatar name={author.name} picture={author.picture} />
         </div>
       )
