792493a28c372f46db15f0e7f3688fcce2e72848
Angus Gaukroger <agaukroger77@gmail.com>
2023-11-21 14:06:22 +0000

    ads.txt + new ad snippet

--- app/layout.js
+++ app/layout.js
@@ -13,8 +13,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4914994958096865"
-        crossorigin="anonymous"></script>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4914994958096865"
+     crossorigin="anonymous"></script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
